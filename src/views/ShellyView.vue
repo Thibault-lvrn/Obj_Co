1c926e1467b4b05fd486149bc5e34d1cc4fa91c6
thibault livran <thibault.livran@dnd.fr>
2023-10-20 12:18:47 +0200

    feat/create toggle button to turn on/off the connected socket

--- src/views/ShellyView.vue
+++ src/views/ShellyView.vue
@@ -1,14 +1,46 @@
 <script setup>
-// fetch de l'api http://192.168.1.100/shelly :
 import { ref } from 'vue'
-import { onMounted } from 'vue';
 
 const shelly = ref('')
+const isActive = ref(true)
 
-onMounted(async () => {
-  const response = await fetch(`http://192.168.1.100/settings/actions`)
-  shelly.value = await response.json()
-})
+// function initialState() {
+//   fetch(`http://192.168.1.100/relay/0/ison`)
+//     .then(response => {
+//       // Gérer la réponse de l'API ici
+//       isActive.value = response.json()
+//     })
+//     .catch(error => {
+//       // Gérer les erreurs ici
+//       console.error('Erreur lors de l\'exécution de l\'action ON :', error);
+//     });
+// }
+
+// initialState();
+
+// setInterval(initialState, 1000);
+
+
+function toggle() {
+  isActive.value = !isActive.value
+
+  const newState = isActive.value;
+  const deviceId = "80646F827174"; // Remplacez par l'ID de votre dispositif.
+  const authKey = "MWRmYzM2dWlkE62C6C4C76F817CE0A3D2902F5B5D4C115E49B28CF8539114D9246505DE5D368D560D06020A92480"; // Remplacez par votre clé d'authentification.
+
+  const toggleUrl = `https://shelly-86-eu.shelly.cloud/device/relay/control?channel=0&turn=${newState ? "on" : "off"}&id=${deviceId}&auth_key=${authKey}`;
+
+  fetch(toggleUrl)
+  .then(response => {
+    // Gérer la réponse de l'API ici
+    console.log('Action ON effectuée avec succès');
+    // isActive.value = !isActive.value
+  })
+  .catch(error => {
+    // Gérer les erreurs ici
+    console.error('Erreur lors de l\'exécution de l\'action ON :', error);
+  });
+}
 
 </script>
 
@@ -16,8 +48,7 @@ onMounted(async () => {
   <div class="about">
     <div class="container">
       <h1>You can control Shelly connected socket here</h1>
-      <button>On</button>
-      <pre>{{ shelly }}</pre>
+      <button :class="{active:isActive}" @click="toggle()">{{ isActive ? 'ON' : 'OFF' }}</button>
     </div>
   </div>
 </template>
@@ -37,7 +68,7 @@ onMounted(async () => {
   }
 
   button {
-    width: 200px;
+    width: 150px;
     margin: 0 auto;
     padding: 10px 30px;
     border-radius: 10px;
