ca067f95f6b40c070685bb330dd5d19772ddeb94
thibault livran <thibault.livran@dnd.fr>
2023-11-20 15:35:30 +0100

    shellyApi: rebase shelly request & toggle state button

--- src/views/ShellyView.vue
+++ src/views/ShellyView.vue
@@ -1,59 +1,69 @@
 <script setup>
-import { ref } from 'vue'
+import axios from 'axios';
 
-const shelly = ref('')
-const isActive = ref(true)
+const id = '083a8dc147d9';
+const authKey = 'MWYwM2Q2dWlkBBBFAC4F49BFB7094B9150C9F212BE1554B887BFEB4C943FACAE317F921C1089805C1285B9DEB2A9';
 
-// function initialState() {
-//   fetch(`http://192.168.1.100/relay/0/ison`)
-//     .then(response => {
-//       // Gérer la réponse de l'API ici
-//       isActive.value = response.json()
-//     })
-//     .catch(error => {
-//       // Gérer les erreurs ici
-//       console.error('Erreur lors de l\'exécution de l\'action ON :', error);
-//     });
-// }
+let data = {};
+let state = false;
 
-// initialState();
+const fetchData = async () => {
+  try {
+    const response = await axios.get(`https://shelly-86-eu.shelly.cloud/device/status?id=${id}&auth_key=${authKey}`);
+    data = response.data.data.device_status;
+    state = data.relays[0].ison;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-// setInterval(initialState, 1000);
+const toggle = () => {
+  const formData = new FormData();
+  formData.append('id', id);
+  formData.append('turn', state ? 'off' : 'on');
+  formData.append('auth_key', authKey);
+  formData.append('channel', '0');
 
+  const headers = formData.getHeaders ? formData.getHeaders() : { 'Content-Type': 'multipart/form-data' };
+  const config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'https://shelly-86-eu.shelly.cloud/device/relay/control',
+    headers: {
+      ...headers,
+    },
+    data: formData,
+  };
 
-function toggle() {
-  isActive.value = !isActive.value
+  axios
+    .request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
-  const newState = isActive.value;
-  const deviceId = "80646F827174"; // Remplacez par l'ID de votre dispositif.
-  const authKey = "MWRmYzM2dWlkE62C6C4C76F817CE0A3D2902F5B5D4C115E49B28CF8539114D9246505DE5D368D560D06020A92480"; // Remplacez par votre clé d'authentification.
+  state = !state;
+};
 
-  const toggleUrl = `https://shelly-86-eu.shelly.cloud/device/relay/control?channel=0&turn=${newState ? "on" : "off"}&id=${deviceId}&auth_key=${authKey}`;
-
-  fetch(toggleUrl)
-  .then(response => {
-    // Gérer la réponse de l'API ici
-    console.log('Action ON effectuée avec succès');
-    // isActive.value = !isActive.value
-  })
-  .catch(error => {
-    // Gérer les erreurs ici
-    console.error('Erreur lors de l\'exécution de l\'action ON :', error);
-  });
-}
-
+fetchData();
 </script>
 
 <template>
   <div class="about">
     <div class="container">
       <h1>You can control Shelly connected socket here</h1>
-      <button :class="{active:isActive}" @click="toggle()">{{ isActive ? 'ON' : 'OFF' }}</button>
+      <button :class="{ active: state }" @click="toggle">{{ state ? 'ON' : 'OFF' }}</button>
+      <ul class="informations">
+        <li>Too hot ? : {{ data.overtemperature ? 'Oui' : 'Non' }}</li>
+        <li>Temperature : {{ data.temperature }}</li>
+      </ul>
     </div>
   </div>
 </template>
 
-<style>
+<style scoped>
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
